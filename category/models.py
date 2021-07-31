babfc5631f0ebff1e06ceb2172f4a134c306e424
TonmoyChak <tuj46842@temple.edu>
2021-07-30 20:14:12 -0400

    made all dropdown functional

--- category/models.py
+++ category/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
@@ -12,6 +12,10 @@ class Category(models.Model):
         # fixing singular plural for category
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args = [self.slug])
+
 
 
     def __str__(self):
