5e9a36318db675801544ca2646a24d7a698dee1f
TonmoyChak <tuj46842@temple.edu>
2021-08-02 21:18:23 -0400

    Cart added

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,12 @@
 
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 
 urlpatterns = [
 
     path('',views.store, name='store'),
+    path('cart/', include('carts.urls')),
     path('<slug:category_slug>/',views.store, name='products_by_category'),
     path('<slug:category_slug>/<slug:product_slug>/',views.product_detail, name='product_detail'),
 
